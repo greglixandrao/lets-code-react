e0b75ab3a2062b45ac7035b4c3e6bcfcc186bf48
Greg Lixandrão <contato@greglixandrao.com>
2021-07-17 19:10:27 -0300

    fim da aula Ciclo de Vida

--- react01/my-app/src/index.js
+++ react01/my-app/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 // import App from "./components/App";
 // import App2 from "./components/App2";
 // import App3 from "./components/App3";
-import App4 from "./components/App4";
+// import App4 from "./components/App4";
+import App5 from "./components/App5";
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App2 title='Titulo'>Conteúdo</App2> */}
     {/* <App2 title='Titulo2'>Conteúdo2</App2> */}
-    <App4 />
+    <App5 />
   </React.StrictMode>,
   document.getElementById("root")
 );
