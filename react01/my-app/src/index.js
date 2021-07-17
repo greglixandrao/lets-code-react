2857579351d0c2a0315ac2869c98db1a67e769bd
Greg Lixandrão <contato@greglixandrao.com>
2021-07-17 20:06:42 -0300

    Arrumando index.js apos merge

--- react01/my-app/src/index.js
+++ react01/my-app/src/index.js
@@ -3,23 +3,15 @@ import ReactDOM from "react-dom";
 // import App from "./components/App";
 // import App2 from "./components/App2";
 // import App3 from "./components/App3";
-<<<<<<< HEAD
 // import App4 from "./components/App4";
 // import Lista from "./components/Lista";
 import Lista2 from "./components/Lista2";
-=======
-import App4 from "./components/App4";
->>>>>>> parent of d571ca5 (Merge pull request #1 from greglixandrao/developer)
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App2 title='Titulo'>Conteúdo</App2> */}
     {/* <App2 title='Titulo2'>Conteúdo2</App2> */}
-<<<<<<< HEAD
     <Lista2 />
-=======
-    <App4 />
->>>>>>> parent of d571ca5 (Merge pull request #1 from greglixandrao/developer)
   </React.StrictMode>,
   document.getElementById("root")
 );
